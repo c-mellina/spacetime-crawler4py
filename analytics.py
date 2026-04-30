a796d5475d54d151db84ca3ad3b9c31b68f9ca08
Christa Mellina Gottardo <cmellina@uci.edu>
2026-04-29 18:44:20 -0700

    feat: add analytics file to track information. Implement word counter, longest page, and unique page tracking in scraper.py. Still need subdomain tracking.

---
+++ analytics.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+unique_pages = set()
+word_counter = Counter()
+longest_page = ("", 0)
+subdomains = {}
--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urlparse, urljoin, urldefrag
 from bs4 import BeautifulSoup
+import analytics
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -22,6 +23,29 @@ def extract_next_links(url, resp):
 
     # resource: https://www.geeksforgeeks.org/python/beautifulsoup-scraping-link-from-html/
     found_urls = []
+    stop_words = stop_words = {
+        "a", "about", "above", "after", "again", "against", "all", "am", "an",
+        "and", "any", "are", "aren't", "as", "at", "be", "because", "been",
+        "before", "being", "below", "between", "both", "but", "by", "can't",
+        "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't",
+        "doing", "don't", "down", "during", "each", "few", "for", "from",
+        "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having",
+        "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+        "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll",
+        "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its",
+        "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no",
+        "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought",
+        "our", "ours", "ourselves", "out", "over", "own", "same", "shan't",
+        "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some",
+        "such", "than", "that", "that's", "the", "their", "theirs", "them",
+        "themselves", "then", "there", "there's", "these", "they", "they'd",
+        "they'll", "they're", "they've", "this", "those", "through", "to", "too",
+        "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll",
+        "we're", "we've", "were", "weren't", "what", "what's", "when", "when's",
+        "where", "where's", "which", "while", "who", "who's", "whom", "why",
+        "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll",
+        "you're", "you've", "your", "yours", "yourself", "yourselves"
+        }
 
     # checking to make sure there was no error
     if resp.status != 200:
@@ -44,6 +68,7 @@ def extract_next_links(url, resp):
     if content_length > 1000000:    # We can change this threshold as seen fit (it is currently 1 million)
         return found_urls
     
+    # get next URLs
     content = BeautifulSoup(resp.raw_response.content, 'lxml')
     for tag in content.find_all("a", href=True):
         new_url = tag["href"]
@@ -54,14 +79,21 @@ def extract_next_links(url, resp):
         new_url = new_url.split("#")[0]
         if new_url:
             found_urls.append(new_url)
-    
-    # Using this content, we can also call content.get_text(strip=True)
-    # Then, we could hopefully split() this string using " " as a delimiter
-    # Then we iterate through each word (perhaps tokenizing it)
-    # We check if this is a stop word, and if not, we can add it to a counter dictionary
-    # This would let us find the 50 most common words
-    # We should also keep track of the number of words
 
+    # parse text and ignore stop words
+    text = content.get_text()
+    words = re.split(r'\W+', text.lower())
+    words = [w for w in words if w and w not in stop_words]
+    
+    # update count dictionary to find most common words
+    analytics.word_counter.update(words)
+
+    # if this content is longer than previous, update it as longest
+    if words.length() > analytics.longest_page[1]:
+        analytics.longest_page = (resp.url, words.length())
+
+    # add the url minus fragment to unique pages set
+    analytics.unique_pages.add(urldefrag(resp.url).url)
 
     return found_urls
 
